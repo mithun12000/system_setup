2ce770f190f90a70ad9de1f2de0105af4dda3ef0
Mithun Mandal <mithun12000@gmail.com>
2016-07-31 01:55:11 +0530

    Update hive.sh

--- hive.sh
+++ hive.sh
@@ -1,2 +1,5 @@
 sudo yum install -y hive hive-metastore hive-server2 hive-hbase mysql-connector-java
 ln -s /usr/share/java/mysql-connector-java.jar /usr/lib/hive/lib/mysql-connector-java.jar
+
+sudo -u hdfs hdfs dfs -chmod -R 770 /user/hive/warehouse
+sudo -u hdfs hdfs dfs -chown -R hive:hive /user/hive/warehouse
