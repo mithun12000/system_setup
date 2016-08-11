748cbb506714f224b97717834965a940b51dedac
Mithun Mandal <mithun12000@gmail.com>
2016-08-11 21:20:44 +0530

    adding ubuntu setup

---
+++ ubuntu/initial.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt-get install -y default-jre
+sudo apt-get install -y default-jdk
+sudo apt-get install -y openjdk-7-jdk
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install -y oracle-java7-installer
+sudo apt-get install -y oracle-java8-installer
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
+sudo apt-get install -y vim git
