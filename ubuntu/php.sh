3801270d9e01aacf5f2a4d1f4caf905f59ff02a4
Mithun Mandal <mithun12000@gmail.com>
2016-08-12 22:02:27 +0530

    Update php.sh

--- ubuntu/php.sh
+++ ubuntu/php.sh
@@ -1,3 +1,3 @@
 sudo apt-get install curl php5-cli git
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-composer config github-oauth.github.com  80fc8a396d88b32ca16c7b5e4a22423894789b01
+composer config github-oauth.github.com  <token>
