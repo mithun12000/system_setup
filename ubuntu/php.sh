fd558697aad90c68954ac890d0e4bc9b3639d8a8
Mithun Mandal <mithun12000@gmail.com>
2016-08-15 13:48:47 +0530

    Update php.sh

--- ubuntu/php.sh
+++ ubuntu/php.sh
@@ -1,3 +1,6 @@
-sudo apt-get install curl php5-cli git
+sudo apt-get install curl php5-cli git libapache2-mod-php5
+sudo apt-get install php5-curl php5-mongo php5-memcache php5-memcached php5-sqlite php5-intl php5-dev php5-mysqlnd 
+sudo apt-get install php5-gd php5-odbc php5-imagick php5-sqlite php5-mcrypt php5-apcu
+sudo apt-get install mysql-server
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 composer config github-oauth.github.com  <token>
